0e9beeb7b90156c3658968c6ee233b9db9086813
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2020-01-17 15:45:22 -0800

    Add files via upload

---
+++ Custom/custom.rb
@@ -0,0 +1,21 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 125
+live_loop :first do
+  play :a2
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :b3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :b3
+  sleep 0.5
+  play :e3
+  sleep 0.5
+  play :b3
+  sleep 0.5
+  
+end
